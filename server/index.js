e583c9a7f2ee59343b64930c592912684d10a54d
bentalla108 <tallambaye108@gmail.com>
2023-09-01 18:51:47 +0000

    Updating secret file and private key

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 // IMPORTS FROM PACKAGES
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+dotenv.config();
 // IMPORTS FROM OTHER FILES
 const authRouter = require("./routes/auth");
 /* const productRouter = require("./routes/product");
@@ -9,8 +11,7 @@ const userRouter = require("./routes/user");
 // INIT
 const PORT = process.env.PORT || 3000;
 const app = express();
-const DB = "mongodb+srv://bentalla:bentalla@cluster0.jwixwj5.mongodb.net/?retryWrites=true&w=majority"
-
+const DB = process.env.DB;
 // middleware
 app.use(express.json());
 app.use(authRouter);
